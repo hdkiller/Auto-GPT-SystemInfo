4e719aeca21612f66207cdc5fc69e7289939f3a3
HDKiller <hdkiller@gmail.com>
2023-04-20 14:06:46 +0200

    system information plugin initial commit

--- src/auto_gpt_plugin_template/__init__.py
+++ src/auto_gpt_plugin_template/__init__.py
@@ -1,7 +1,9 @@
 """This is a template for Auto-GPT plugins."""
 import abc
-from typing import Any, Dict, List, Optional, Tuple, TypeVar, TypedDict
+import platform
+from typing import Any, Dict, List, Optional, Tuple, TypedDict, TypeVar
 
+import distro
 from abstract_singleton import AbstractSingleton, Singleton
 
 PromptGenerator = TypeVar("PromptGenerator")
@@ -12,16 +14,70 @@ class Message(TypedDict):
     content: str
 
 
-class AutoGPTPluginTemplate(AbstractSingleton, metaclass=Singleton):
+class AutoGPTPluginSystemInfo(AbstractSingleton, metaclass=Singleton):
     """
     This is a template for Auto-GPT plugins.
     """
 
     def __init__(self):
         super().__init__()
-        self._name = "Auto-GPT-Plugin-Template"
+        self._name = "Auto-GPT-Plugin-SystemInfo"
         self._version = "0.1.0"
-        self._description = "This is a template for Auto-GPT plugins."
+        self._description = "This is system info plugin for Auto-GPT."
+
+    def get_system_info(self) -> str:
+        """
+        Gets system information.
+
+        Returns:
+            str: The system information.
+        """
+
+        # Get system architecture
+        arch = platform.architecture()[0]
+
+        # Get system distribution (works on Linux only)
+        if platform.system() == "Linux":
+            distro_name = distro.name()
+            distro_version = distro.version()
+            distro_id = distro.id()
+
+            distro_info = f"{distro_name} {distro_version} ({distro_id})"
+        else:
+            distro_info = None
+
+        # Get Windows version (works on Windows only)
+        if platform.system() == "Windows":
+            win_ver = platform.win32_ver()
+            win_version = f"{win_ver[0]} {win_ver[1]} {win_ver[4]}"
+        else:
+            win_version = None
+
+        # Get macOS version (works on macOS only)
+        if platform.system() == "Darwin":
+            mac_ver = platform.mac_ver()
+            mac_version = f"{mac_ver[0]} {mac_ver[2]}"
+        else:
+            mac_version = None
+
+        # Build the prompt
+        if distro_info:
+            os_info = f"Linux {arch} {distro_info}"
+        if win_version:
+            os_info = f"Windows {win_version}"
+        if mac_version:
+            os_info = f"macOS {mac_version}"
+
+        return os_info
+
+    def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
+        os_info = self.get_system_info()
+        if os_info:
+            prompt.add_resource(
+                f"You are running {os_info}. ",
+            )
+
+        return prompt
 
     @abc.abstractmethod
     def can_handle_on_response(self) -> bool:
@@ -44,7 +100,7 @@ class AutoGPTPluginTemplate(AbstractSingleton, metaclass=Singleton):
 
         Returns:
             bool: True if the plugin can handle the post_prompt method."""
-        return False
+        return True
 
     @abc.abstractmethod
     def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
